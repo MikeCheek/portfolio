7d3a3bf746a610a244118527331f28056b7d9c27
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-06-20 21:28:14 +0200

    Added profile animation

--- src/atoms/photoProfile/photoProfile.tsx
+++ src/atoms/photoProfile/photoProfile.tsx
@@ -2,43 +2,10 @@ import React from 'react'
 import {StaticImage} from 'gatsby-plugin-image'
 
 import * as styles from './photoProfile.module.scss'
-// import detectBrowser from '../../utilities/detectBrowser'
-//import { useThemeContext } from '../../utilities/themeContext'
 
 const PhotoProfile = (): JSX.Element => {
-  //const theme: String = useThemeContext();
-
-  // const parallax = (event: MouseEvent): void => {
-  //   const shift: HTMLElement = document.getElementById('profile')!
-  //   const rect: DOMRect = shift.getBoundingClientRect()
-  //   const middleY: number = rect.top + rect.height / 2
-  //   const middleX: number = rect.left + rect.width / 2
-  //   const x: number = (middleX - event.pageX) / 10
-  //   const y: number = -(middleY - event.pageY) / 10
-
-  //   shift.style.transform = ` perspective(300px) rotateX(${y}deg) rotateY(${x}deg) scale(1.05)`
-  // }
-
-  // const addListener = (): void | null =>
-  //   detectBrowser() != 'Safari' ? document.addEventListener('mousemove', parallax) : null
-  // const removeListener = (): void => {
-  //   document.removeEventListener('mousemove', parallax)
-  //   const shift = document.getElementById('profile')
-  //   if (shift) shift.style.removeProperty('transform')
-  // }
-
-  // //create the handleclick function
-  // const handleClick = () => {}
-
   return (
-    <div
-      id="profile"
-      // onMouseEnter={addListener}
-      // onMouseLeave={removeListener}
-      // onMouseOver={addListener}
-      // onMouseOut={removeListener}
-      // onClick={handleClick}
-    >
+    <div id="profile" className={styles.profile}>
       <StaticImage
         src={'../../images/newProfescional.png'}
         alt="Michele Pulvirenti"
@@ -51,6 +18,12 @@ const PhotoProfile = (): JSX.Element => {
         height={200}
         className={styles.profescional}
       />
+      <div className={styles.circles}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   )
 }
