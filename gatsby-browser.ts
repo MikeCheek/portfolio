3f2594b5af2c32f1f4eea809522f669ceda13aea
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-06-28 14:54:33 +0200

    Fixeing service worker

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,3 +1,13 @@
 // // import 'firebase/auth'
 // import 'firebase/firestore'
 // import 'firebase/functions'
+
+export const registerServiceWorker = () => true
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(`This application has been updated. ` + `Reload to display the latest version?`)
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
