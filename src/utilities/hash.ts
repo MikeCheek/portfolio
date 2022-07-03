e2a9e9b4a4c7cd84d2c4b6d0a14d1480759be1b5
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-07-03 21:20:55 +0200

    Improving hash

--- src/utilities/hash.ts
+++ src/utilities/hash.ts
@@ -1,7 +1,8 @@
 export const hashToStr = (hash: string) => {
+  let n = 2
   const arrHash = Array.from(hash)
     .reverse()
-    .map((char) => prevLetter(char))
+    .map((char) => prevLetter(char, n))
   const str = arrHash.join('')
   return [str.substring(0, str.length - 1).toUpperCase(), str.charAt(str.length - 1)]
 }
@@ -9,15 +10,16 @@ export const hashToStr = (hash: string) => {
 export const strToHash = (str: string, lang: string = 'en') => {
   const l = lang === 'en' ? 'e' : 'i'
   const array = Array.from(str + l)
-  const arrHash = array.map((char) => nextLetter(char)).reverse()
+  let n = 2
+  const arrHash = array.map((char) => nextLetter(char, n)).reverse()
   const hash = arrHash.join('')
   return hash.toUpperCase()
 }
 
-const nextLetter = (letter: string) => {
-  return String.fromCharCode(letter.charCodeAt(letter.length - 1) + 1)
+const nextLetter = (letter: string, n: number = 1) => {
+  return String.fromCharCode(letter.charCodeAt(letter.length - 1) + n)
 }
 
-const prevLetter = (letter: string) => {
-  return String.fromCharCode(letter.charCodeAt(letter.length - 1) - 1)
+const prevLetter = (letter: string, n: number = 1) => {
+  return String.fromCharCode(letter.charCodeAt(letter.length - 1) - n)
 }
