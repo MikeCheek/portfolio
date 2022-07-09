03303c8eb0b55d9c3223e7d75de0ca74995616c0
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-07-09 20:36:44 +0200

    Removed carousel autofocus

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -51,7 +51,6 @@ const Projects = () => {
             />
           )}
           showThumbs={false}
-          autoFocus
           useKeyboardArrows
           stopOnHover
           showIndicators={false}
