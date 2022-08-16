d8e22388ab69a0d4e03c2bd6a163869614883eb6
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-08-16 19:33:39 +0200

    Added rotating world

---
+++ src/atoms/rotatingWorld/rotatingWorld.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as styles from './rotatingWorld.module.scss'
+import {RotatingWorldProps} from './rotatingWorld.types'
+
+const RotatingWorld = ({children}: RotatingWorldProps) => {
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.circle}></div>
+      <div className={styles.circle} style={{opacity: 0.7, animationDelay: '50ms'}}></div>
+      <div className={styles.circle} style={{opacity: 0.4, animationDelay: '100ms'}}></div>
+      <div className={styles.circle} style={{opacity: 0.1, animationDelay: '150ms'}}></div>
+      {children}
+    </div>
+  )
+}
+
+export default RotatingWorld
---
+++ src/atoms/rotatingWorld/rotatingWorld.types.ts
@@ -0,0 +1,3 @@
+export interface RotatingWorldProps {
+  children: React.ReactNode
+}
