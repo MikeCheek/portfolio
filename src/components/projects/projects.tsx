52fc94cd516c1a63cd34898157030989c726cbb1
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-08-16 16:35:35 +0200

    Revert "Update projects.tsx"
    This reverts commit 2d77a57890fc76a5be218f74430454b8196b6c38.

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -58,6 +58,7 @@ const Projects = () => {
           showIndicators={false}
           emulateTouch
           showStatus={false}
+          infiniteLoop
           swipeScrollTolerance={10}
           preventMovementUntilSwipeScrollTolerance
         >
