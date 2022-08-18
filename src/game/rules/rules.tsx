c5e2394b970d1253adf60a93e215394c90bf9be3
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2022-08-18 14:12:39 +0200

    Fixed rules

--- src/game/rules/rules.tsx
+++ src/game/rules/rules.tsx
@@ -10,14 +10,14 @@ const Rules = () => {
         <br />
         <br />
         <li>
-          When you guess a letter, the box turns <span style={{color: 'var(--orange)'}}>orange</span>
+          When you guess a letter of the word, the box turns <span style={{color: 'var(--orange)'}}>orange</span>
         </li>
         <li>
           When you guess both the letter and the position in the word, the box turns{' '}
           <span style={{color: 'var(--pink)'}}>pink</span>
         </li>
         <br />
-        You can change the MAX length and the language of the hidden word at the top. <br />
+        You can change the MAX length and language of the hidden word and other things in the settings. <br />
         <br />
         That's all! Enjoy the game!
       </h4>
