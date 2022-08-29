d9f15503ffefa045cda50a4a2ba53d2eb8b247a2
Michele Pulvirenti <62357072+MikeCheek@users.noreply.github.com>
2022-08-29 18:24:09 +0200

    Update info.tsx

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -23,7 +23,7 @@ const InfoPage = () => {
 
   return (
     <>
-      <SEO title="Loading" description="Just multiple test" pathname="/info/" />
+      <SEO title="Info" description="Just multiple test" pathname="/info/" />
       <Layout>
         <>
           <Loading />
