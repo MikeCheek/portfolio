4b20714862460666fd9c9b1672e9e499f1d3ddc6
Michele Pulvirenti <62357072+MikeCheek@users.noreply.github.com>
2022-08-30 13:07:28 +0200

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Link} from 'gatsby'
 import SEO from '../components/seo/seo'
 import Layout from '../components/layout/layout'
 import Rocket from '../components/rocket/rocket'
 
 const NotFoundPage = (): JSX.Element => {
+  
+  const [bored, setBored] useState<JSON>(JSON.parse(`{"loading": "data"}`))
+  const [error, setError] = useState<boolean>(false)
+  
+  const fetchData = async () => {
+    const result: Response = await fetch('https://www.boredapi.com/api/activity')
+    
+    if (result.ok) {
+      const d: JSON = await result.json()
+      setBored(d)
+    } else {
+      setError(true)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
 
   return (
     <>
@@ -26,6 +45,12 @@ const NotFoundPage = (): JSX.Element => {
           <Link to="/" className="buttonStyle">
             GO HOME
           </Link>
+          {!error && (
+            <div>
+              <p>If you are bored, try {bored.link==""? bored.activity : <a target="_blank" href={bored.link}>{bored.activity}</a>}</p>
+            </div>
+          )}
+          
         </span>
       </Layout>
     </>
