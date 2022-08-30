f70cbae990e123ee6e6de49c0e27e4395e19c535
Michele Pulvirenti <62357072+MikeCheek@users.noreply.github.com>
2022-08-30 12:50:31 +0200

    Removed comments

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,24 +2,9 @@ import React from 'react'
 import {Link} from 'gatsby'
 import SEO from '../components/seo/seo'
 import Layout from '../components/layout/layout'
-// import ReactGA from 'react-ga4'
-
 import Rocket from '../components/rocket/rocket'
-// import {collection, doc, updateDoc, increment} from 'firebase/firestore'
-// import {database} from '../../firebaseConfig'
 
 const NotFoundPage = (): JSX.Element => {
-  // const dbInstance = collection(database, 'pages')
-  // const fs = doc(dbInstance, '404')
-  // const update = async () => {
-  //   await updateDoc(fs, {
-  //     times: increment(1),
-  //   })
-  // }
-
-  // useEffect(() => {
-  //   update()
-  // }, [])
 
   return (
     <>
