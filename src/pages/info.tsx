3c8d6e87d94207939141805c1e1d5ff2ef4646f0
Michele Pulvirenti <62357072+MikeCheek@users.noreply.github.com>
2022-09-01 17:18:37 +0200

    Added deploys info

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,48 +1,68 @@
-import React, {useEffect, useState} from 'react'
-import Loading from '../atoms/loading/loading'
-import Layout from '../components/layout/layout'
-import SEO from '../components/seo/seo'
+import React, { useEffect, useState } from 'react';
+import Loading from '../atoms/loading/loading';
+import Layout from '../components/layout/layout';
+import SEO from '../components/seo/seo';
 
 const InfoPage = () => {
-  const [data, setData] = useState<JSON>(JSON.parse(`{"loading": "data"}`))
-  const [error, setError] = useState<boolean>(false)
+  const [data, setData] = useState<JSON>();
+  const [deploys, setDeploys] = useState<JSON>();
+  const [error, setError] = useState<boolean[]>([false, false]);
+  const [fetched, setFetched] = useState<boolean[]>([false, false]);
 
-  const fetchData = async () => {
-    const result: Response = await fetch('https://api.github.com/repos/mikecheek/portfolio/languages')
-    if (result.ok) {
-      const d: JSON = await result.json()
-      setData(d)
-    } else {
-      setError(true)
-    }
-  }
+  const fetchData = () => {
+    fetch('https://api.github.com/repos/mikecheek/portfolio/languages')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((_err) => setError((err) => [true, err[1]]))
+      .finally(() => setFetched((fetch) => [true, fetch[1]]));
+
+    fetch('https://api.github.com/repos/mikecheek/portfolio/actions/workflows/20375826/runs')
+      .then((response) => response.json())
+      .then((data) => setDeploys(data))
+      .catch((_err) => setError((err) => [err[0], true]))
+      .finally(() => setFetched((fetch) => [fetch[0], true]));
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <>
       <SEO title="Info" description="Just multiple test" pathname="/info/" />
       <Layout>
         <>
-          <Loading />
-          {!error ? (
-            <div>
-              <p>There are {Object.keys(data!).length} different languages on this website </p>
-              {Object.keys(data!).map((entry: string, key: number) => {
-                return (
-                  <p key={key}>
-                    {entry}: {(data as any)[entry]}
-                  </p>
-                )
-              })}
-            </div>
-          ) : null}
+          {fetched[0] ? (
+            !error[0] && (
+              <div>
+                <p>There are {Object.keys(data!).length} different languages on this website </p>
+                {Object.keys(data!).map((entry: string, key: number) => {
+                  return (
+                    <p key={key}>
+                      {entry}: {(data as any)[entry]}
+                    </p>
+                  );
+                })}
+              </div>
+            )
+          ) : (
+            <Loading />
+          )}
+
+          {fetched[1] ? (
+            !error[1] && (
+              <div>
+                <p>This site was built and deployed {deploys['total_count']} times</p>
+              </div>
+            )
+          ) : (
+            <Loading />
+          )}
         </>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default InfoPage
+export default InfoPage;
+
