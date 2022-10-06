844d25a965e917d4417d51f60cb193a77a542d3d
MikeCheek <michele00.pulvirenti@gmail.com>
2022-10-06 11:38:10 +0200

    Another try

--- src/atoms/threeD/index.tsx
+++ src/atoms/threeD/index.tsx
@@ -5,13 +5,28 @@ import Box from './box'
 import {TOUCH} from 'three'
 
 const Index = () => {
+  const handleOneTouch = (event: React.TouchEvent<HTMLDivElement>) => {
+    event.preventDefault()
+  }
+
+  const handleTouch = (event: React.TouchEvent<HTMLDivElement>) => {
+    switch (event.touches.length) {
+      case 1: {
+        handleOneTouch(event)
+        break
+      }
+      default:
+        break
+    }
+  }
+
   return (
-    <div>
+    <div onTouchStart={(e) => handleTouch(e)} onTouchMove={(e) => handleTouch(e)}>
       <Canvas>
         <Box />
         <ambientLight intensity={0.2} />
         <directionalLight position={[-2, 5, 2]} intensity={0.2} />
-        <OrbitControls enableZoom={false} enablePan={false} autoRotate touches={{ONE: TOUCH.PAN, TWO: TOUCH.ROTATE}} />
+        <OrbitControls enableZoom={false} autoRotate touches={{ONE: TOUCH.DOLLY_ROTATE, TWO: TOUCH.ROTATE}} />
       </Canvas>
     </div>
   )
