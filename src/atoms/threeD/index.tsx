ebc3fe935bdd3339983df3245267ebd760f7599f
MikeCheek <michele00.pulvirenti@gmail.com>
2022-10-06 17:47:57 +0200

    Trying to fix 3d touch scroll

--- src/atoms/threeD/index.tsx
+++ src/atoms/threeD/index.tsx
@@ -5,16 +5,16 @@ import Box from './box'
 import {TOUCH} from 'three'
 
 const Index = () => {
-  const handleOneTouch = (event: React.TouchEvent<HTMLDivElement>) => {
-    event.stopPropagation()
-  }
-
-  const handleTouch = (event: React.TouchEvent<HTMLDivElement>) => {
-    if (event.touches.length == 1) handleOneTouch(event)
+  const handleTouchMove = (event: React.TouchEvent<HTMLDivElement>) => {
+    if (event.touches.length == 1) {
+      event.stopPropagation()
+      const current = event.touches[0].clientY
+      window.scroll(0, current)
+    }
   }
 
   return (
-    <div onTouchStart={(e) => handleTouch(e)} onTouchMove={(e) => handleTouch(e)}>
+    <div onTouchMove={(e) => handleTouchMove(e)}>
       <Canvas>
         <Box />
         <ambientLight intensity={0.2} />
