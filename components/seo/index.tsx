b8527a538c3cb279f43f9bf72b65860f0c95cabc
Mike <michele00.pulvirenti@gmail.com>
2023-05-01 23:25:55 +0200

    Added structured data

--- components/seo/index.tsx
+++ components/seo/index.tsx
@@ -3,6 +3,7 @@ import NextHeadSeo from "next-head-seo"
 import config from "../../properties"
 //import { useStaticQuery, graphql } from "gatsby"
 import {SEOProps /*QueryTypes*/} from "./index.types"
+import Head from "next/head"
 
 const Index = ({
   description = "",
@@ -18,43 +19,110 @@ const Index = ({
   const url: string = config.url
   const image: string = url + "/logo.png"
   const canonical = pathname ? `${config.siteUrl}${pathname}` : null
+  const name = "Michele Pulvirenti"
+
+  const shcemaOrgPerson = {
+    "@type": "Person",
+    "@id": url,
+    name: name,
+    alternateName: name,
+    nationality: "Italian",
+    gender: "Male",
+    Description: "Developer and student",
+    // disambiguatingDescription: "",
+    jobTitle: "Developer",
+    email: "pulvirentimichele00@gmail.com",
+    url: url,
+    image: image,
+    sameAs: ["https://www.linkedin.com/in/michele-pulvirenti/", "https://github.com/MikeCheek"],
+  }
+
+  const schemaOrgWebPage = {
+    "@type": "WebSite",
+    url: url,
+    inLanguage: "en",
+    mainEntityOfPage: url,
+    description: metaDescription,
+    name: defaultTitle,
+    author: {
+      "@type": "Person",
+      name: name,
+    },
+    copyrightHolder: {
+      "@type": "Person",
+      name: name,
+    },
+    copyrightYear: "2023",
+    creator: {
+      "@type": "Person",
+      name: name,
+    },
+    publisher: {
+      "@type": "Person",
+      name: name,
+    },
+    datePublished: new Date(Date.now()).toISOString(),
+    dateModified: new Date(Date.now()).toISOString(),
+    image: {
+      "@type": "ImageObject",
+      url: image,
+    },
+  }
 
   return (
-    <NextHeadSeo
-      title={title}
-      description={metaDescription}
-      canonical={canonical ?? url}
-      og={{
-        title: defaultTitle,
-        description: metaDescription,
-        url: url,
-        image: image,
-        type: "website",
-        siteName: title,
-      }}
-      twitter={{
-        card: image ? "summary_large_image" : "summary",
-      }}
-      customMetaTags={[
+    <>
+      <NextHeadSeo
+        title={title}
+        description={metaDescription}
+        canonical={canonical ?? url}
+        og={{
+          title: defaultTitle,
+          description: metaDescription,
+          url: url,
+          image: image,
+          type: "website",
+          siteName: title,
+        }}
+        twitter={{
+          card: image ? "summary_large_image" : "summary",
+        }}
+        customMetaTags={[
+          {
+            name: `google-site-verification`,
+            content: googleSiteVerification ?? "",
+          },
+          {
+            name: `msvalidate.01`,
+            content: bingSiteVerification ?? "",
+          },
+          {
+            property: "og:image:width",
+            content: "200px",
+          },
+          {
+            property: "og:image:height",
+            content: "200px",
+          },
+          ...meta,
+        ]}
+      />
+      <Head>
+        <script
+          type="application/ld+json"
+          key="product-jsonld"
+          dangerouslySetInnerHTML={{
+            __html: `
         {
-          name: `google-site-verification`,
-          content: googleSiteVerification ?? "",
-        },
-        {
-          name: `msvalidate.01`,
-          content: bingSiteVerification ?? "",
-        },
-        {
-          property: "og:image:width",
-          content: "200px",
-        },
-        {
-          property: "og:image:height",
-          content: "200px",
-        },
-        ...meta,
-      ]}
-    />
+          "@context": "http://schema.org",
+          "@graph": [
+          ${JSON.stringify(shcemaOrgPerson)}, 
+          ${JSON.stringify(schemaOrgWebPage)}      
+        ]           
+        }`,
+          }}
+        ></script>
+      </Head>
+    </>
   )
 }
 
