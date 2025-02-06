31cb789f71d45daac9fabf1c0a3e240c0ead3c29
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2025-02-06 15:21:02 +0100

    Lowed quality and duration on segmented video

--- utilities/info.ts
+++ utilities/info.ts
@@ -233,7 +233,7 @@ export const projectsList: Project[] = [
     
     Project developed for Machine Learning for CV course at ESILV.`,
     github: "https://github.com/MikeCheek/Semantic-Segmentation-of-Underwater-Imagery",
-    video: require("../assets/videos/segmented.mp4"),
+    video: require("../assets/videos/segmented-low.mp4"),
     technologies: ["Python", "OpenCV", "PyTorch", "MatplotLib", "NumPy"],
     tools: ["Machine Learning for Computer Vision", "Segmentation models"],
   },
