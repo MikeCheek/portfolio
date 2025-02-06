21c8b392e3b6b1e7c393ada61da69917e615b0f5
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2025-02-06 19:05:29 +0100

    Other fixes

--- components/atoms/Banner/index.tsx
+++ components/atoms/Banner/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import styles from './index.module.scss'
-import BannerProps from './index.types'
+import React from "react"
+import styles from "./index.module.scss"
+import BannerProps from "./index.types"
 
-const Index = ({ text }: BannerProps) => {
+const Index = ({text}: BannerProps) => {
   return (
-    <a className={styles.banner} href={text.includes('href=') ? text.match(/href="([^"]*)"/)![1] : '#'} target="_blank" rel="noopener noreferrer">
-      <span dangerouslySetInnerHTML={{ __html: text }}></span>
-    </a>)
+    <a
+      className={styles.banner}
+      href={text.includes("href=") ? text.match(/href="([^"]*)"/)![1] : "#"}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <span dangerouslySetInnerHTML={{__html: text}}></span>
+    </a>
+  )
 }
 
 export default Index
