21a73a7f8ddb0122734f5dea766b3048b233664a
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2025-07-02 16:32:11 +0200

    fixed, let's go

--- components/molecules/PageTransition/index.tsx
+++ components/molecules/PageTransition/index.tsx
@@ -1,40 +1,40 @@
 import {motion} from "framer-motion"
-import styles from "./index.module.scss"
+// import styles from "./index.module.scss"
 import {PageTransitionProps} from "./index.types"
 import {forwardRef} from "react"
 
-const pageVariants = {
-  initial: {
-    rotateY: 100,
-    skewY: 4,
-    scale: 0.98,
-    opacity: 0,
-    transformOrigin: "center left",
-    boxShadow: "0 0 0 rgba(0,0,0,0)",
-  },
-  animate: {
-    rotateY: 0,
-    skewY: 0,
-    scale: 1,
-    opacity: 1,
-    transition: {
-      duration: 0.4,
-      ease: "easeIn",
-    },
-  },
-  exit: {
-    rotateY: -100,
-    skewY: 4,
-    scale: 0.98,
-    opacity: 0,
-    transformOrigin: "center right",
-    boxShadow: "-50px 0 80px rgba(0,0,0,0.2)",
-    transition: {
-      duration: 0.4,
-      ease: "easeIn",
-    },
-  },
-}
+// const pageVariants = {
+//   initial: {
+//     rotateY: 100,
+//     skewY: 4,
+//     scale: 0.98,
+//     opacity: 0,
+//     transformOrigin: "center left",
+//     boxShadow: "0 0 0 rgba(0,0,0,0)",
+//   },
+//   animate: {
+//     rotateY: 0,
+//     skewY: 0,
+//     scale: 1,
+//     opacity: 1,
+//     transition: {
+//       duration: 0.4,
+//       ease: "easeIn",
+//     },
+//   },
+//   exit: {
+//     rotateY: -100,
+//     skewY: 4,
+//     scale: 0.98,
+//     opacity: 0,
+//     transformOrigin: "center right",
+//     boxShadow: "-50px 0 80px rgba(0,0,0,0.2)",
+//     transition: {
+//       duration: 0.4,
+//       ease: "easeIn",
+//     },
+//   },
+// }
 
 const Index = forwardRef(({children}: PageTransitionProps) => {
   return (
@@ -51,5 +51,7 @@ const Index = forwardRef(({children}: PageTransitionProps) => {
     </motion.main>
   )
 })
+
+Index.displayName = "PageTransition"
 
 export default Index
