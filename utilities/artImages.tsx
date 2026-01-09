e500f741051bf208e7aae15e96270ece4bb6f60f
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2026-01-09 16:43:36 +0100

    update art painting

--- utilities/artImages.tsx
+++ utilities/artImages.tsx
@@ -29,6 +29,12 @@ export type ArtImage = {
 
 export const paintings: ArtImage[] = [
   {
+    img: "/images/painting/20260107.jpg",
+    title: "Santa Maria la Scala",
+    date: "2026-01-07",
+    badges: [BADGES.ACRILYC],
+  },
+  {
     img: "/images/painting/20251210.jpg",
     title: "Oriental Night",
     date: "2025-12-10",
