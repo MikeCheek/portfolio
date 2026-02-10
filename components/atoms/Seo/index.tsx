04b1735f33a1e31b57b4f0ddffb9ab0f28aa91e9
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2026-02-10 17:46:08 +0100

    Add Cookiebot script for consent management in SEO component

--- components/atoms/Seo/index.tsx
+++ components/atoms/Seo/index.tsx
@@ -136,6 +136,13 @@ const Index = ({
         }`,
           }}
         ></script>
+        <script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="c84fc9aa-4627-4323-8c9a-151289867272"
+          data-blockingmode="auto"
+          type="text/javascript"
+        ></script>
       </Head>
     </>
   )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,16 +3,7 @@ import {Html, Head, Main, NextScript} from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Global font imports or icons go here */}
-        <script
-          id="Cookiebot"
-          src="https://consent.cookiebot.com/uc.js"
-          data-cbid="c84fc9aa-4627-4323-8c9a-151289867272"
-          data-blockingmode="auto"
-          type="text/javascript"
-        ></script>
-      </Head>
+      <Head>{/* Global font imports or icons go here */}</Head>
       <body>
         <Main />
         <NextScript />
