0741341660b77640703f01d876341d4448f9b777
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2026-02-11 12:23:02 +0100

    Last try

--- pages/cookies.tsx
+++ pages/cookies.tsx
@@ -7,28 +7,11 @@ const Cookies = () => {
   return (
     <>
       <SEO title="Cookies Policy" description="Information about cookies used on the website." pathname="/cookies/" />
-      <Layout noBackground>
-        <div style={{maxWidth: "800px", margin: "0 auto", padding: "40px 20px"}}>
-          <h1>Cookies Policy</h1>
-          <div
-            style={{
-              marginTop: "40px",
-              width: "90vw",
-              maxWidth: "800px",
-              marginLeft: "auto",
-              marginRight: "auto",
-              border: "0",
-              borderTop: "1px solid var(--orange)",
-            }}
-          >
-            <Script
-              id="CookieDeclaration"
-              src="https://consent.cookiebot.com/c84fc9aa-4627-4323-8c9a-151289867272/cd.js"
-              type="text/javascript"
-            ></Script>
-          </div>
-        </div>
-      </Layout>
+      <Script
+        id="CookieDeclaration"
+        src="https://consent.cookiebot.com/c84fc9aa-4627-4323-8c9a-151289867272/cd.js"
+        type="text/javascript"
+      ></Script>
     </>
   )
 }
