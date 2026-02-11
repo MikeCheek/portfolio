0b144d2cf6d869cee14675ffcda855e544e04167
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2026-02-11 12:11:55 +0100

    Fixed cookie consent page

--- pages/cookies.tsx
+++ pages/cookies.tsx
@@ -9,12 +9,24 @@ const Cookies = () => {
       <Layout noBackground>
         <div style={{maxWidth: "800px", margin: "0 auto", padding: "40px 20px"}}>
           <h1>Cookies Policy</h1>
-          <script
-            id="CookieDeclaration"
-            src="https://consent.cookiebot.com/c84fc9aa-4627-4323-8c9a-151289867272/cd.js"
-            type="text/javascript"
-            async
-          ></script>
+          <div
+            style={{
+              marginTop: "40px",
+              width: "90vw",
+              maxWidth: "800px",
+              marginLeft: "auto",
+              marginRight: "auto",
+              border: "0",
+              borderTop: "1px solid var(--orange)",
+            }}
+          >
+            <script
+              id="CookieDeclaration"
+              src="https://consent.cookiebot.com/c84fc9aa-4627-4323-8c9a-151289867272/cd.js"
+              type="text/javascript"
+              async
+            ></script>
+          </div>
         </div>
       </Layout>
     </>
